1b8fac3ceb1880cbf23bedc5290f2385ab31cb99
Matt Sinisi <matt.sinisi@coenterprise.com>
2018-12-09 15:17:42 -0500

    .

--- dist/js/configure_app.js
+++ dist/js/configure_app.js
@@ -1,13 +1,10 @@
 // This file is for configuring the various pieces of the application for working against your server
 
 window.AppConfig = {
-    server_url: "https://tableau.coenterprise.com",
+    server_url: "https://tableaudev.coenterprise.com",
     username: "matt.sinisi",
     site_name: "Sandbox",
-    password: "Rhmit0321!", // Let's be honest, you should never do this.  Get the password from the user.
-    team_tag: "portal",
-    cors: {
-        ip: "127.0.0.1",
-        port: 9999
-    }
+    password: "portal", // Let's be honest, you should never do this.  Get the password from the user.
+    team_tag: "portal"
+
 };
